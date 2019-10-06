51841d5a9c3da441c9bac250eda11c362da05bc6
elan33 <67664518@qq.com>
2019-10-07 07:46:51 +0800

    hax

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -162,8 +162,8 @@ TARGET_RIL_VARIANT := caf
 # SELinux
 include device/qcom/sepolicy-legacy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += \
-    device/huawei/kiwi/sepolicy
+#BOARD_SEPOLICY_DIRS += \
+#    device/huawei/kiwi/sepolicy
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
